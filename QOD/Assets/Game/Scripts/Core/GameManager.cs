a9a136388af97fe943310b75985a1bacbc2b1760
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2026-05-07 23:22:29 +0530

    Update GameManager

--- QOD/Assets/Game/Scripts/Core/GameManager.cs
+++ QOD/Assets/Game/Scripts/Core/GameManager.cs
@@ -1,9 +1,38 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[DefaultExecutionOrder(-100)]
 public class GameManager : MonoSingleton<GameManager>
 {
+    [SerializeField] private GameDataManager _gameDataManager;
+    [SerializeField] private SaveDataManager _saveDataManager;
+
+    private void OnEnable()
+    {
+        _gameDataManager.Load();
+        RegisterServices();
+        _saveDataManager.Load();
+    }
+
+    private void OnDisable()
+    {
+        UnregisterServices();
+    }
+
+    private void RegisterServices()
+    {
+        ServiceLocator.Register(_gameDataManager);
+        ServiceLocator.Register(_saveDataManager);
+    }
+
+    private void UnregisterServices()
+    {
+        ServiceLocator.Unregister<GameDataManager>();
+        ServiceLocator.Unregister<SaveDataManager>();
+    }
+
     public void ExitLevel()
     {
 
